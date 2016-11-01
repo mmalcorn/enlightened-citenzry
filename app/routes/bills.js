e5e929d3f84169953d9eb246a7549e02a874ae00
Lisa MacCarrigan and Meredith Alcorn <lisa+meredith@>
2016-11-01 16:29:16 -0400

    restructure bills..js to handle multiple promises for api requests, add each-in loops to sort through and display search results

--- app/routes/bills.js
+++ app/routes/bills.js
@@ -2,11 +2,35 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Route.extend({
+
   model: function(params) {
+
+    var resultsBucket = [];
+
+    var getNextPageJSON = function(newURL) {
+        return Ember.$.getJSON(newURL).then(function(responseJSON) {
+          resultsBucket.push(responseJSON.results);
+          // console.log(resultsBucket);
+        });
+    };
+
+
     var key = config.myApiKey;
-    var url = 'http://congress.api.sunlightfoundation.com/bills/search?apikey=' +key+ '&last_version_on__gte=' + params.date;
+    var url = 'http://congress.api.sunlightfoundation.com/bills/search?apikey=' +key+ '&per_page=50&page=1&last_version_on__gte=' + params.date;
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.results;
+      var count = responseJSON.count;
+      // var resultsBucket = [];
+
+      for(var pageNumber = 1; pageNumber * 50 < count; pageNumber++) {
+
+        var newURL = 'http://congress.api.sunlightfoundation.com/bills/search?apikey=' +key+ '&per_page=50&page=' +pageNumber+ '&last_version_on__gte=' + params.date;
+
+        getNextPageJSON(newURL);
+      }
+      return resultsBucket;
     });
   }
 });
+
+
+//cd70edbb9f004458ba61e71e6c90e152
